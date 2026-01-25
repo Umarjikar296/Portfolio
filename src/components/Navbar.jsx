f4f2862c32c5144a59b2c05400558f48dc1feb25
Ashwin Umarjikar <ashwin@Mac.fritz.box>
2026-01-25 21:25:22 +0100

    Fix build: resolve logo asset path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import Logo from '../assets/logo.png'
+import Logo from '../assets/Logo.png'
 import { useEffect, useRef, useState } from 'react';
 import OverlayMenu from './OverlayMenu';
 import { IoMdMenu } from "react-icons/io";
